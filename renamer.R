dffe7c3409f217024c0abaed615a15161626177c
George Moroz <agricolamz@gmail.com>
2019-04-07 08:35:07 +0300

    add scripts

---
+++ renamer.R
@@ -0,0 +1,20 @@
+path <- "..."
+setwd(path)
+library(tidyverse)
+df <- read_csv("questionary.csv")
+
+dictor <- "2019.04.05_f_d11"
+path <- paste0(path, "/", dictor)
+
+df %>% 
+  arrange(word) %>% 
+  mutate(pair_id = as.character(pair_id),
+         pair_id = ifelse(nchar(pair_id) == 1, paste0("0", pair_id), pair_id),
+         file_name = paste0(dictor, "_", pair_id, "_", word, ".wav"),
+         file_name = str_replace_all(file_name, "а́", "А"),
+         file_name = str_replace_all(file_name, "ы́", "Ы"),
+         file_name = str_replace_all(file_name, "о́", "О")) ->
+  df
+
+file.rename(paste0(path, "/", list.files(path)),
+            paste0(path, "/", df$file_name))
